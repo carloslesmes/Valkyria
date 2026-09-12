387e3f6cb168d91c610b6b309228b4adf200e274
Carlos LESMES <carloslesmes@gmail.com>
2026-09-12 15:40:25 -0500

    Delete server.R

--- server.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-shinyServer(function(input, output){
-  output$distPlot <- renderPlot({
-    curve(dweibull(x,input$a,input$b),
-          from = 0, to = 2*input$b, col = "darkblue",
-          xlab = "Variable Aleatoria Positiva",
-          ylab = "Densidad Weibull", main = "Distribución de Weibull")
-  })
-  })
